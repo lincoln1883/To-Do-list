494a01dcaca4c8f8f474b526344e0828d62fa628
lincoln gibson <lincolngibson7@gmail.com>
2023-05-03 13:28:06 -0300

    Add task to and update task with localstorage.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,32 @@ import returnIcon from './assets/return.svg';
 
 // Create a function to display the task list
 
-const task = [
-  {
-    id: 1,
-    name: 'Learn React',
-    completed: false,
-  },
-  {
-    id: 2,
-    name: 'Learn Node',
-    completed: false,
-  },
-  {
-    id: 3,
-    name: 'Learn SQL',
-    completed: false,
-  },
-  {
-    id: 4,
-    name: 'Learn Express',
-    completed: true,
-  },
-  {
-    id: 5,
-    name: 'Mongo DB',
-    completed: true,
-  },
+let task = JSON.parse(localStorage.getItem('tasks')) || [
+	{
+		id: 1,
+		name: 'Learn React',
+		completed: false,
+	},
+	{
+		id: 2,
+		name: 'Learn Node',
+		completed: false,
+	},
+	{
+		id: 3,
+		name: 'Learn SQL',
+		completed: false,
+	},
+	{
+		id: 4,
+		name: 'Learn Express',
+		completed: true,
+	},
+	{
+		id: 5,
+		name: 'Mongo DB',
+		completed: true,
+	},
 ];
 
 const todoList = document.querySelector('.todo-list');
@@ -46,30 +46,35 @@ returnImg.classList.add('return-icon');
 returnImg.setAttribute('src', returnIcon);
 todoContainer.appendChild(returnImg);
 
+// function for displaying the list of tasks
 const displayTask = () => {
-  todoList.innerHTML = '';
-  task.sort((a, b) => a.id - b.id);
+	todoList.innerHTML = '';
+	task.sort((a, b) => a.id - b.id);
 
-  task.forEach((task) => {
-    const taskItem = document.createElement('li');
-    const div = document.createElement('div');
-    div.classList.add('task');
-    const input = document.createElement('input');
-    input.setAttribute('type', 'checkbox');
-    input.checked = task.completed;
-    const icon = document.createElement('img');
-    icon.classList.add('option-icon');
-    icon.setAttribute('src', dots);
-    const span = document.createElement('span');
-    span.innerText = task.name;
-    div.appendChild(input);
-    div.appendChild(span);
-    div.appendChild(icon);
-    taskItem.appendChild(div);
-    todoList.appendChild(taskItem);
-  });
+	task.forEach((task, id) => {
+		const taskItem = document.createElement('li');
+		const div = document.createElement('div');
+		div.classList.add('task');
+		const input = document.createElement('input');
+		input.setAttribute('type', 'checkbox');
+		input.checked = task.completed;
+		const icon = document.createElement('img');
+		icon.classList.add('option-icon');
+		icon.setAttribute('src', dots);
+		const span = document.createElement('span');
+		span.innerText = task.name;
+		div.appendChild(input);
+		div.appendChild(span);
+		div.appendChild(icon);
+		taskItem.appendChild(div);
+		todoList.appendChild(taskItem);
+
+		task.id = id + 1;
+	});
+
+	localStorage.setItem('tasks', JSON.stringify(task));
 };
 
 window.onload = () => {
-  displayTask();
+	displayTask();
 };
