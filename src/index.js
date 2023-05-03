0ca96490845ca3ca6091252e35c13def08cfabbc
lincoln gibson <lincolngibson7@gmail.com>
2023-05-03 12:07:39 -0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,32 +5,32 @@ import returnIcon from './assets/return.svg';
 
 // Create a function to display the task list
 
-let task = [
-	{
-		id: 1,
-		name: 'Learn React',
-		completed: false,
-	},
-	{
-		id: 2,
-		name: 'Learn Node',
-		completed: false,
-	},
-	{
-		id: 3,
-		name: 'Learn SQL',
-		completed: false,
-	},
-	{
-		id: 4,
-		name: 'Learn Express',
-		completed: true,
-	},
-	{
-		id: 5,
-		name: 'Mongo DB',
-		completed: true,
-	},
+const task = [
+  {
+    id: 1,
+    name: 'Learn React',
+    completed: false,
+  },
+  {
+    id: 2,
+    name: 'Learn Node',
+    completed: false,
+  },
+  {
+    id: 3,
+    name: 'Learn SQL',
+    completed: false,
+  },
+  {
+    id: 4,
+    name: 'Learn Express',
+    completed: true,
+  },
+  {
+    id: 5,
+    name: 'Mongo DB',
+    completed: true,
+  },
 ];
 
 const todoList = document.querySelector('.todo-list');
@@ -47,29 +47,29 @@ returnImg.setAttribute('src', returnIcon);
 todoContainer.appendChild(returnImg);
 
 const displayTask = () => {
-	todoList.innerHTML = '';
-	task.sort((a, b) => a.id - b.id);
+  todoList.innerHTML = '';
+  task.sort((a, b) => a.id - b.id);
 
-	task.forEach((task) => {
-		let taskItem = document.createElement('li');
-		let div = document.createElement('div');
-		div.classList.add('task');
-		let input = document.createElement('input');
-		input.setAttribute('type', 'checkbox');
-		input.checked = task.completed;
-		let icon = document.createElement('img');
-		icon.classList.add('option-icon');
-		icon.setAttribute('src', dots);
-		let span = document.createElement('span');
-		span.innerText = task.name;
-		div.appendChild(input);
-		div.appendChild(span);
-		div.appendChild(icon);
-		taskItem.appendChild(div);
-		todoList.appendChild(taskItem);
-	});
+  task.forEach((task) => {
+    const taskItem = document.createElement('li');
+    const div = document.createElement('div');
+    div.classList.add('task');
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.checked = task.completed;
+    const icon = document.createElement('img');
+    icon.classList.add('option-icon');
+    icon.setAttribute('src', dots);
+    const span = document.createElement('span');
+    span.innerText = task.name;
+    div.appendChild(input);
+    div.appendChild(span);
+    div.appendChild(icon);
+    taskItem.appendChild(div);
+    todoList.appendChild(taskItem);
+  });
 };
 
 window.onload = () => {
-	displayTask();
+  displayTask();
 };
