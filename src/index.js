5900fc1bfa5f6081e7303d67d2f7390fe7aa9233
lincoln gibson <lincolngibson7@gmail.com>
2023-05-06 10:11:59 -0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -30,105 +30,105 @@ const deleteBtn = document.querySelector('.delete-btn');
 
 // function for displaying the list of tasks
 const saveTasks = () => {
-	localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 const displayTask = () => {
-	todoList.innerHTML = '';
+  todoList.innerHTML = '';
 
-	tasks.forEach((task, index) => {
-		task.index = index + 1;
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
 
-		const taskItem = document.createElement('li');
-		taskItem.innerHTML = `
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = `
     <div class="task">
       <input type="checkbox" class="checkbox" data-task-id="task-${
-				task.index
-			}" ${task.completed ? 'checked' : ''}>
+  task.index
+}" ${task.completed ? 'checked' : ''}>
       <span class="task-label" for="task-${task.index}">${task.name}</span>
       <img class="option-icon" src=${dots}>
       </div>
       `;
-		todoList.appendChild(taskItem);
+    todoList.appendChild(taskItem);
 
-		const taskLabel = taskItem.querySelector('.task-label');
+    const taskLabel = taskItem.querySelector('.task-label');
 
-		const checkbox = taskItem.querySelector(
-			`[data-task-id="task-${task.index}"]`
-		);
-		if (checkbox) {
-			checkbox.addEventListener('click', (e) => {
-				task.completed = checkbox.checked;
-				if (task.completed) {
-					deleteBtn.classList.remove('disabled');
-				} else {
-					deleteBtn.classList.add('disabled');
-				}
-				saveTasks();
-				displayTask();
-			});
-		}
+    const checkbox = taskItem.querySelector(
+      `[data-task-id="task-${task.index}"]`,
+    );
+    if (checkbox) {
+      checkbox.addEventListener('click', () => {
+        task.completed = checkbox.checked;
+        if (task.completed) {
+          deleteBtn.classList.remove('disabled');
+        } else {
+          deleteBtn.classList.add('disabled');
+        }
+        saveTasks();
+        displayTask();
+      });
+    }
 
-		const taskDots = taskItem.querySelector('.option-icon');
-		taskDots.addEventListener('click', () => {
-			const taskId = task.index;
-			const taskIndex = tasks.findIndex((task) => task.index === taskId);
+    const taskDots = taskItem.querySelector('.option-icon');
+    taskDots.addEventListener('click', () => {
+      const taskId = task.index;
+      const taskIndex = tasks.findIndex((task) => task.index === taskId);
 
-			const newInput = document.createElement('input');
-			newInput.classList.add('edit-input');
-			newInput.setAttribute('type', 'text');
-			newInput.value = tasks[taskIndex].name;
+      const newInput = document.createElement('input');
+      newInput.classList.add('edit-input');
+      newInput.setAttribute('type', 'text');
+      newInput.value = tasks[taskIndex].name;
 
-			const trashIcon = document.createElement('img');
-			trashIcon.classList.add('trash-icon');
-			trashIcon.setAttribute('src', trash);
+      const trashIcon = document.createElement('img');
+      trashIcon.classList.add('trash-icon');
+      trashIcon.setAttribute('src', trash);
 
-			taskLabel.replaceWith(newInput);
-			taskDots.replaceWith(trashIcon);
+      taskLabel.replaceWith(newInput);
+      taskDots.replaceWith(trashIcon);
 
-			newInput.focus();
+      newInput.focus();
 
-			newInput.addEventListener('change', () => {
-				const taskName = newInput.value;
-				tasks = editTask(tasks, taskId, taskName);
-				saveTasks();
-				displayTask();
-			});
+      newInput.addEventListener('change', () => {
+        const taskName = newInput.value;
+        tasks = editTask(tasks, taskId, taskName);
+        saveTasks();
+        displayTask();
+      });
 
-			trashIcon.addEventListener('click', () => {
-				removeTask(taskId, tasks);
-				saveTasks();
-				displayTask();
-			});
-		});
-	});
+      trashIcon.addEventListener('click', () => {
+        removeTask(taskId, tasks);
+        saveTasks();
+        displayTask();
+      });
+    });
+  });
 };
 
 form.addEventListener('submit', addTask);
 
 submitBtn.addEventListener('click', (e) => {
-	e.preventDefault();
-	const taskName = taskInput.value;
-	const existingTaskId = parseInt(taskInput.dataset.taskId, 10) || null;
-	tasks = addTask(tasks, taskName, existingTaskId);
-	displayTask();
-	saveTasks();
-	taskInput.value = '';
-	taskInput.dataset.taskId = '';
+  e.preventDefault();
+  const taskName = taskInput.value;
+  const existingTaskId = parseInt(taskInput.dataset.taskId, 10) || null;
+  tasks = addTask(tasks, taskName, existingTaskId);
+  displayTask();
+  saveTasks();
+  taskInput.value = '';
+  taskInput.dataset.taskId = '';
 });
 
 deleteBtn.addEventListener('click', () => {
-	tasks = completedTask(tasks);
-	saveTasks();
-	displayTask();
+  tasks = completedTask(tasks);
+  saveTasks();
+  displayTask();
 });
 
 image.addEventListener('click', (e) => {
-	e.target.classList.toggle('rotate');
-	saveTasks();
-	displayTask();
+  e.target.classList.toggle('rotate');
+  saveTasks();
+  displayTask();
 });
 
 window.onload = () => {
-	displayTask();
+  displayTask();
 };
