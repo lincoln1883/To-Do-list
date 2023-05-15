46049f89b56e34f768b2956d8704b9bbf28e93e5
lincoln gibson <lincolngibson7@gmail.com>
2023-05-14 21:29:49 -0300

    Edit function to resolve index updating bug.

--- src/removeTask.js
+++ src/removeTask.js
@@ -1,13 +1,15 @@
 const removeTask = (taskId, tasks) => {
-  const taskIndex = tasks.findIndex((task) => task.index === taskId);
-  if (taskIndex !== -1) {
-    tasks.sort((a, b) => a - b).splice(taskIndex, 1);
+	if (tasks && tasks.length > 0) {
+		const taskIndex = tasks.findIndex((task) => task.index === taskId);
+		if (taskIndex !== -1) {
+			tasks.splice(taskIndex, 1);
 
-    for (let i = taskIndex; i < tasks.length; i += 1) {
-      tasks[i].index = i + 1;
-    }
-  }
-  return tasks;
+			for (let i = taskIndex; i < tasks.length; i += 1) {
+				tasks[i].index = tasks[i].index - 1;
+			}
+		}
+	}
+	return tasks;
 };
 
 export default removeTask;
